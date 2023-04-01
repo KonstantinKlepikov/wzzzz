8cccb1875f2f833b0b8bbf839ff0e31e84b72fd7
Konstantin Klepikov <oformleno@gmail.com>
2023-04-01 02:14:44 +0200

    delete template

--- bot/app/app/handlers/templates.py
+++ bot/app/app/handlers/templates.py
@@ -21,14 +21,33 @@ button_names = [
     'ten'
         ]
 
+async def delete_template(
+    c: CallbackQuery,
+    button: Button,
+    dialog_manager: DialogManager
+        ) -> None:
+    """Delete template
+    """
+    user_id = c.from_user.id
+    template = dialog_manager.dialog_data.get('template')
+    if template:
+        try:
+            await dialog_manager.middleware_data["qm"].delete_template(user_id, template['name'])
+            await c.answer(f'Is deleted template: {template["name"]}')
+        except HttpError as e:
+            await c.answer(e.message)
+    else:
+        c.answer(f'You must choose template to delete.')
+
+
 async def get_template_fields(**kwargs) -> str:
     """Get template fields
     """
     template = kwargs['dialog_manager'].dialog_data['template']
     result = 'Template fields' \
-            f'\nname: {template.get("name", "-")}' \
-            f'\nexpirience: {template.get("expirience", "-")}' \
-            f'\ntext: {template.get("text", "-")}'
+            f'\nname: {template.get("name") or "-"}' \
+            f'\nexpirience: {template.get("expirience") or "-"}' \
+            f'\ntext: {template.get("text") or "-"}'
     return {'text': result}
 
 
@@ -38,9 +57,9 @@ template_window = Window(
     Column(
         Button(Const('query for vacancies'), id='query_for_vacancies'),
         Button(Const('change template fields'), id='change_template_fields'),
-        Button(Const('delete templates'), id='delete_template'),
+        Button(Const('delete template'), id='delete_template', on_click=delete_template),
             ),
-    Back(Const('back to templates')),
+    Back(Const('back to list of templates')),
     Cancel(Const('exit')),
     state=StartGrp.template,
     getter=get_template_fields
